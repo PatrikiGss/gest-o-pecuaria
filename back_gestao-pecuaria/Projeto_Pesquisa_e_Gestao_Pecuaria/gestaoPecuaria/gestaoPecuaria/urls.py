8e10e82695159db7dc4ff2626be09ec575cac0ca
PatrikiGss <patriki7771@gmail.com>
2024-09-19 18:51:18 -0300

    adição do caminho de accounts ao urls.py raiz do projeto

--- back_gestao-pecuaria/Projeto_Pesquisa_e_Gestao_Pecuaria/gestaoPecuaria/gestaoPecuaria/urls.py
+++ back_gestao-pecuaria/Projeto_Pesquisa_e_Gestao_Pecuaria/gestaoPecuaria/gestaoPecuaria/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls', namespace='core')),
+    path('', include('core.urls', namespace='core')),  # Rotas do app 'core'
+    path('accounts/', include('accounts.urls', namespace='accounts')),  # Rotas do app 'accounts'
 ]
